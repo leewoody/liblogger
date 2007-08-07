013f4bceced472b1ca08139eba471fc167847b92
dpa-toolkit <dpa-toolkit@84f246e8-0537-0410-8d96-9892f03ad600>
2007-08-07 06:45:18 +0000

    new header indicating the log level prefix added

---
+++ src/inc/loglevel_prefix.h
@@ -0,0 +1,8 @@
+#ifndef __LOG_LEVEL_PREFIX_H__
+#define __LOG_LEVEL_PREFIX_H__
+
+#define LOG_LEVEL_INFO_PREFIX 		"[I]"
+#define LOG_LEVEL_DEBUG_PREFIX 		"[D]"
+#define LOG_LEVEL_CRITICAL_PREFIX 	"[C]"
+
+#endif // __LOG_LEVEL_PREFIX_H__
