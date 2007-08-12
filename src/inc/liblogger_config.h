31c7cbb1387cc33192b279c60d7318e1875c479e
dpa-toolkit <dpa-toolkit@84f246e8-0537-0410-8d96-9892f03ad600>
2007-08-12 13:06:24 +0000

    macros DISABLE_VARIADIC_CHECK,  ASSUME_VARIADIC_SUPPORT, DISABLE_FILENAMES added.

--- src/inc/liblogger_config.h
+++ src/inc/liblogger_config.h
@@ -4,4 +4,19 @@
 /** define this macro to disable all logs */
 //#define DISABLE_ALL_LOGS
 
+/** Define this macro to prevent checking the variadic macro support of your compiler.
+ * By defining this macro, you can also stop the filenames, function name, and the line no
+ * from appearing in the logs.
+ * */
+//#define DISABLE_VARIADIC_CHECK
+
+/** Define this macro to forcefully enable the varadic macro support. This can lead to compile time
+ * errors if the compiler doesnot support variadic macros.*/
+//#define ASSUME_VARIADIC_SUPPORT
+
+/** Define this macro to prevent filenames from appearing in the logs. This is useful if the filenames are very long
+ * and their presence is not desired in the logs.
+ * */
+//#define DISABLE_FILENAMES
+
 #endif // __LIBLOGGER_CONFIG_H__
