c4efe8ecd83eed8cc750ac93e5dc3527e71161d0
dpa-toolkit <dpa-toolkit@84f246e8-0537-0410-8d96-9892f03ad600>
2007-08-12 11:38:44 +0000

    deleting...

--- src/inc/loglevel_prefix.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LOG_LEVEL_PREFIX_H__
-#define __LOG_LEVEL_PREFIX_H__
-
-#define LOG_LEVEL_INFO_PREFIX 		"[I]"
-#define LOG_LEVEL_DEBUG_PREFIX 		"[D]"
-#define LOG_LEVEL_CRITICAL_PREFIX 	"[C]"
-
-#endif // __LOG_LEVEL_PREFIX_H__
