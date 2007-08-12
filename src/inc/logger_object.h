cb4ba0d264367f1e8623f414f53db9cb23b59a95
dpa-toolkit <dpa-toolkit@84f246e8-0537-0410-8d96-9892f03ad600>
2007-08-12 13:08:29 +0000

    signature of Log function changed, more doc added.

--- src/inc/logger_object.h
+++ src/inc/logger_object.h
@@ -3,12 +3,10 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include  "liblogger.h"
-/** Indicates the logger object. */
 
 /* fwd declaration. */
 struct LogWriter;
-/** Indicates the log writer object. */
-typedef int (*Log)(struct LogWriter* _this,const char* levelStr,
+typedef int (*Log)(struct LogWriter* _this,const LogLevel logLevel,
 #ifdef VARIADIC_MACROS
 		const char* moduleName,
 		const char* file,const char* funcName, const int lineNum, 
@@ -21,9 +19,14 @@ typedef int (*LoggerDeInit)(struct LogWriter* _this);
 /** The log writer object */
 typedef struct LogWriter
 {
+	/** Member function to log. */
 	Log 			log;
+	/** Member function to log the function entry. */
 	LogFuncEntry 	logFuncEntry;
+	/** Member function to log the function exit. */
 	LogFuncExit		logFuncExit;
+	/** Member function to deinitialize the log writer object, the log writer object will
+	 * not be referenced after this call.*/
 	LoggerDeInit	loggerDeInit;
 }LogWriter;
 
