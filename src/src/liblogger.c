feccc1da22a7de60101b4d3ab6537f7369b6f8ac
dpa-toolkit <dpa-toolkit@84f246e8-0537-0410-8d96-9892f03ad600>
2007-08-15 10:47:24 +0000

    macro support...

--- src/src/liblogger.c
+++ src/src/liblogger.c
@@ -23,7 +23,7 @@ static tLLMutex	sMutex = 0;
  * */
 #define CHECK_AND_INIT_LOGGER	if(!pLogWriter)	\
 	{ 											\
-		fprintf(stderr,"liblogger not initialized, logging will be done to console (stdout)\n");\
+		fprintf(stderr,"\n[liblogger]liblogger not initialized, logging will be done to console (stdout)\n");\
 		if(InitLogger(LogToConsole)) 			\
 			return -1; 							\
 		if(!pLogWriter)							\
@@ -36,7 +36,7 @@ int InitLogger(LogDest ldest,...)
 {
 	if(pLogWriter)
 	{
-		fprintf(stderr,"Deinitializing the current log writer");
+		fprintf(stderr,"\n [liblogger]Deinitializing the current log writer\n");
 		DeInitLogger();
 	}
 #ifndef DISABLE_THREAD_SAFETY
@@ -45,6 +45,7 @@ int InitLogger(LogDest ldest,...)
 	switch(ldest)
 	{
 		case LogToSocket:
+			#ifndef DISABLE_SOCKET_LOGGER
 			{
 				/* Then 2nd arg is the server and the 3rd ard is the port */
 				va_list args;
@@ -56,11 +57,17 @@ int InitLogger(LogDest ldest,...)
 				va_end(args);
 				if( -1 == InitSocketLogger(&pLogWriter,server,port) )
 				{
-					fprintf(stderr,"\n could not init socket logging, will be done to a file. \n");
+					fprintf(stderr,"\n [liblogger] could not init socket logging, will be done to a file. \n");
 					ldest = LogToFile;
 					goto LOG_TO_FILE;
 				}
 			}
+			#else
+			{
+				fprintf(stderr,"\n [liblogger] Socket logger not enabled during build, use DISABLE_SOCKET_LOGGER=0, logging will be done to file.\n");
+				goto LOG_TO_FILE;
+			}
+			#endif
 			break;
 
 		case LogToConsole:
@@ -69,7 +76,7 @@ int InitLogger(LogDest ldest,...)
 				if( -1 == InitConsoleLogger(&pLogWriter,0) )
 				{
 					// control should never reach here, this should alwasy succeed.
-					fprintf(stderr,"\n could not initialize console logger \n");
+					fprintf(stderr,"\n [liblogger] could not initialize console logger \n");
 					return -1;
 				}
 			}
@@ -86,7 +93,7 @@ LOG_TO_FILE:
 
 				if( -1 == InitFileLogger(&pLogWriter,filename) )
 				{
-					fprintf(stderr,"\n could not initialize file logger, check file path/name \n");
+					fprintf(stderr,"\n [liblogger] could not initialize file logger, check file path/name \n");
 					return -1;
 				}
 			}
