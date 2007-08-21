acc8fd60d6bb9d7628fac2620b44b12d5e660535
dpa-toolkit <dpa-toolkit@84f246e8-0537-0410-8d96-9892f03ad600>
2007-08-21 12:28:01 +0000

    append mode and rollback mode added.

--- src/inc/liblogger/file_logger.h
+++ src/inc/liblogger/file_logger.h
@@ -4,11 +4,16 @@
 #include <stdio.h>
 
 /** File open mode.
- * \todo currently not supported, will be added in future.
  * */
 typedef enum tFileOpenMode
 {
-	__unused,
+	/** Indicates that the log file should be opened in append mode. */
+	AppendMode,
+	/** Rollback Mode : The log file will be opened in append mode and will
+	 * roll back when the size indicated 
+	 * by \ref tFileLoggerInitParams::rollbackSize is reached.
+	 * */
+	RollbackMode
 } tFileOpenMode;
 
 /** File Logger Initialization parameters. */
@@ -18,6 +23,10 @@ typedef struct tFileLoggerInitParams
 	char* 			fileName;
 	/** The file open mode. */
 	tFileOpenMode 	fileOpenMode;
+	/** The rollback size to use, when \ref tFileLoggerInitParams::fileOpenMode "fileOpenMode"
+	 * is equal \ref tFileOpenMode::RollbackMode "RollbackMode"
+	 * */
+	unsigned long	rollbackSize;
 }tFileLoggerInitParams;
 
 #endif // __FILE_LOGGER_H__
