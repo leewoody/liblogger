16b932521166f7ea5081c5f9e7777f452cb61928
dpa-toolkit <dpa-toolkit@84f246e8-0537-0410-8d96-9892f03ad600>
2007-08-21 14:13:21 +0000

    adding Platform layer implementation of socket.

---
+++ src/src/platform_layer/inc/tPLSocket.h
@@ -0,0 +1,31 @@
+/**
+ * \file Platform Layer for socket functions.
+ * */
+#ifndef __PLSOCKET_H__
+#define __PLSOCKET_H__
+
+/** Abstract handle for socket. */
+typedef void* tPLSocket;
+
+/** Function to create a \b connected socket.
+ * \param [in]  server 	The log server.
+ * \param [in]  port	The log server port.
+ * \param [out] sock	The socket handle.
+ * \returns -1 on failure and \a sock will be -1 , or else 0 on success and sock will be non zero.
+ * */
+int CreateConnectedSocket(const char* server, int port, tPLSocket *sock);
+
+/** Send data over socket.
+ * \param [in] tPLSocket 	The socket handle created via \ref CreateConnectedSocket.
+ * \param [in] data			The data to send. 
+ * \param [in] dataSize		The size of data.
+ * \return On success, the amount of bytes sent, -1 otherwise.
+ * */
+int SockSend(tPLSocket sock,const void* data,const int dataSize);
+
+/** Close the Socket.
+ * \param [in,out] sock	The socket handle created via \ref CreateConnectedSocket.
+ * */
+void DestroySocket(tPLSocket * sock);
+
+#endif // __PLSOCKET_H__
