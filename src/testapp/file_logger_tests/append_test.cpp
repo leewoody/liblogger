674f361a1b122b3a4bfb292355bbef436ff1038c
dpa-toolkit <dpa-toolkit@84f246e8-0537-0410-8d96-9892f03ad600>
2007-08-21 15:08:13 +0000

    adding file logger tests.

---
+++ src/testapp/file_logger_tests/append_test.cpp
@@ -0,0 +1,57 @@
+#include <liblogger/liblogger.h>
+#include <liblogger/file_logger.h>
+#include <liblogger/socket_logger.h>
+#include <memory.h>
+
+//#define TEST_APPEND
+
+void TestLogToFile();
+void TestLogFuncs();
+
+int main()
+{
+	// log to a file in append mode.
+	TestLogToFile();
+}
+
+void TestLogToFile()
+{
+	tFileLoggerInitParams fileInitParams;
+	// very important, memset to prevent breaks when new members are
+	// added to fileInitParams.
+	memset(&fileInitParams,0,sizeof(tFileLoggerInitParams));
+	fileInitParams.fileName = "log.log";
+	#ifdef TEST_APPEND
+	fileInitParams.fileOpenMode = AppendMode;
+	#else
+	fileInitParams.fileOpenMode = RollbackMode;
+	fileInitParams.rollbackSize = 10 * 1024; /* 10 KB */
+	#endif
+	InitLogger(LogToFile,&fileInitParams);
+	TestLogFuncs();
+	DeInitLogger();
+}
+
+	
+void TestLogFuncs()
+{
+	LogWarn("Warning message......");
+	LogDebug(" debug log  %#x", 8909);
+
+	// crash testing, test for buffer overflow vulnerability, in case of socket logging, this huge log
+	// will be truncated.
+	LogDebug("abcdefghijklmnopqrstuvwxyz"
+		"abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz"
+		"abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz"
+		"abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz"
+		"abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz"
+		"abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz"
+		"abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz"
+		"abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz-abcdefghijklmnopqrstuvwxyz"
+		);
+
+	LogInfo(" Log Info ");
+	LogTrace("Log Trace ....... \n");
+	LogFatal (" Testing LogFatal.......");
+
+}
