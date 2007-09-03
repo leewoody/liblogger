1be494cea986b19f2bacff884eb2319c57a29cdf
dpa-toolkit <dpa-toolkit@84f246e8-0537-0410-8d96-9892f03ad600>
2007-09-03 09:04:53 +0000

    win32_support.h header file added.

---
+++ src/src/win32_support.h
@@ -0,0 +1,9 @@
+#ifndef __WIN32_SUPPORT_H__
+#define __WIN32_SUPPORT_H__
+
+#if defined(WIN32) || defined(_WIN32)
+#define __func__ __FUNCTION__
+#define snprintf _snprintf
+#endif
+
+#endif // __WIN32_SUPPORT_H__
